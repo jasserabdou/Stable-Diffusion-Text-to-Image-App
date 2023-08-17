8d2bff01047ce94691f29b19f696c3a4e337dbc6
Jasser Abdelfattah <139123140+jasserabdou@users.noreply.github.com>
2023-08-17 16:50:06 +0300

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,3 @@
-import streamlit as st
 import torch
 from accelerate import Accelerator
 from diffusers import StableDiffusionPipeline
@@ -6,12 +5,15 @@ from diffusers import StableDiffusionPipeline
 MODEL_ID = "CompVis/stable-diffusion-v1-4"
 ACCELERATOR = Accelerator()
 
+
+
 def load_model():
     with ACCELERATOR.device:
         pipe = StableDiffusionPipeline.from_pretrained(
             MODEL_ID, torch_dtype=torch.float32)
-        pipe.model.to(ACCELERATOR.device)  # Move the model to the device
+        pipe = pipe.to(ACCELERATOR.device)
     return pipe
+
 
 def main():
     st.title('Stable Diffusion Text-to-Image App 🚀')
@@ -28,9 +30,7 @@ def main():
 
                 try:
                     with torch.no_grad(), ACCELERATOR.device:
-                        prompt_tensor = torch.tensor([prompt]).to(ACCELERATOR.device)
-                        image = pipe(prompt_tensor).images[0]
-                        
+                        image = pipe(prompt).images[0]
                         st.success('Image generated successfully!')
                         st.image(
                             image,
@@ -46,5 +46,6 @@ def main():
                     st.error('Image generation failed. Please try again later.')
                     st.exception(e)
 
+
 if __name__ == "__main__":
-    main()
+    main(
